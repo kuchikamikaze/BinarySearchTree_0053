8dc17b463d43f8ddd52f3c4c29432348d3e01aaa
Naufal Arif <na713634@gmail.com>
2026-06-11 18:22:20 +0700

    fungtion postordertravers

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -95,4 +95,21 @@ class BinaryTree
                 preorder(ptr->rightchild);
             }
         }
+        void postorder(Node *ptr){
+        if (ROOT == NULL)
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+
+        if (ptr != NULL)
+        {
+            postorder(ptr->leftchild);
+            postorder(ptr->rightchild);
+            cout << ptr->info << " ";
+        }
+    }
+
+};
+
 
